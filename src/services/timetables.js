f722f8831ef4745df2757fe0e592eaa3b9dc5afd
Nicolas Gres <nicolas.gres@kemweb.de>
2019-08-05 19:04:18 +0200

    upgradet to React Native 0.60.4 on ios

--- src/services/timetables.js
+++ src/services/timetables.js
@@ -2,11 +2,12 @@ import encoding from 'text-encoding';
 
 export function getHtmlTimetables(url) {
     return new Promise((resolve, reject) => {
+        global.Blob = null
         const request = new XMLHttpRequest();
         const decoder = new encoding.TextDecoder('windows-1252', { NONSTANDARD_allowLegacyEncoding: true });
 
         request.onload = () => {
-            if (request.status === 200) { 
+            if (request.status === 200) {
                 resolve(decoder.decode(request.response));
             } else {
                 reject(new Error(statusText));
